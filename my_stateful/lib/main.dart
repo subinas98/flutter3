399f5df61c1f25fc84885f0ce7751a3d90cce7aa
subinas98 <subinas98@gmail.com>
2022-12-05 12:02:32 +0530

    commit before first run

--- my_stateful/lib/main.dart
+++ my_stateful/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -9,7 +12,10 @@ void main(){
 }
 
 class MyButton extends StatefulWidget{
-
+  @override 
+  MyButtonState createState(){
+    return MyButtonState();
+  }
 }
 
 class MyButtonState extends State<MyButton>{
@@ -18,4 +24,41 @@ class MyButtonState extends State<MyButton>{
     "Uno", "Dos", "Tres", "Cautro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Dietz"
   ];
   String DefaultText = "Spanish Numbers";
+
+  void displaynumbers(){
+    setState(() {
+      DefaultText = SpanishNumbers[counter];
+      if (counter < 9) {
+        counter++;
+      } else {
+        counter = 0;
+      }
+    });
+  }
+
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My stateful app'),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        child: Center(
+          child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Text(DefaultText, style: TextStyle(fontSize: 30.0)),
+              Padding(padding: EdgeInsets.all(10.0),),
+              MaterialButton(
+                child: Text('Call numbers', style: TextStyle(color: Colors.white),),
+                onPressed: displaynumbers,
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
